113d4e5474dbc491b9e2f66d7412655ae1d6634c
Giorgio Grassini <giorgiograssini@gmail.com>
2018-08-11 12:32:49 +0200

    boarding added 🛫

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { RouterModule } from '@angular/router';
 import {routes} from './components/routes';
 import { HandButtonComponent } from './components/hand-button/hand-button.component';
 import { SpaceshipComponent } from './components/spaceship/spaceship.component';
+import { BoardingComponent } from './components/boarding/boarding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HandButtonComponent,
     SpaceshipComponent,
+    BoardingComponent,
   ],
   imports: [
     BrowserModule,
--- client/src/app/components/routes.ts
+++ client/src/app/components/routes.ts
@@ -2,12 +2,14 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HandButtonComponent } from './hand-button/hand-button.component';
 import { SpaceshipComponent } from './spaceship/spaceship.component';
+import { BoardingComponent } from './boarding/boarding.component';
 
 
 
 export const routes: Routes = [
         { path: 'handButton', component: HandButtonComponent},
-        { path: 'spaceship', component: SpaceshipComponent}
+        { path: 'spaceship', component: SpaceshipComponent},
+        { path: 'boarding', component: BoardingComponent}
       ];
 
 
