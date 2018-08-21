02ad51151157aac8a4bd85b562f4c78827529ed4
Giorgio Grassini <giorgiograssini@gmail.com>
2018-08-21 08:35:06 +0200

    Transition jeep to handButton 🔝

--- client/src/app/components/jeep/jeep.component.ts
+++ client/src/app/components/jeep/jeep.component.ts
@@ -1,46 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-jeep',
-  templateUrl: './jeep.component.html',
-  styleUrls: ['./jeep.component.css']
+  selector: "app-jeep",
+  templateUrl: "./jeep.component.html",
+  styleUrls: ["./jeep.component.css"]
 })
 export class JeepComponent implements OnInit {
-
   public lottieConfig: Object;
   private anim: any;
   private animationSpeed: number = 1;
+  public name = "";
+  public surname = "";
+  public next = false;
+  public submit = false;
 
-  constructor() {
-      this.lottieConfig = {
-          path: '../../../assets/jeep/jeep.json',
-          autoplay: true,
-          loop: true
-      };
+  constructor(public route:Router) {
+    this.lottieConfig = {
+      path: "../../../assets/jeep/jeep.json",
+      autoplay: false,
+      loop: false
+    };
   }
 
-  ngOnInit(){
+  ngOnInit() {
+      
+  }
 
+  onEnter(name: string, surname: string) {
+    this.name = name;
+    this.surname = surname;
+    this.submit = true;
+    this.next = true;
+  }
+
+  onConfirm(){
+    this.play();
+    setTimeout (()=>{
+        this.route.navigate(['/handButton'])
+    },3000)
   }
 
   handleAnimation(anim: any) {
-      this.anim = anim;
+    this.anim = anim;
   }
 
   stop() {
-      this.anim.stop();
+    this.anim.stop();
   }
 
   play() {
-      this.anim.play();
+    this.anim.play();
   }
 
   pause() {
-      this.anim.pause();
+    this.anim.pause();
   }
 
   setSpeed(speed: number) {
-      this.animationSpeed = speed;
-      this.anim.setSpeed(speed);
+    this.animationSpeed = speed;
+    this.anim.setSpeed(speed);
   }
 }
