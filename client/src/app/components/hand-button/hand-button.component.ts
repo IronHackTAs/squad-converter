e940291269e17c7b2d28bd191c1de42c897ef710
Juan Sánchez Mesa <sanchezmesaj@gmail.com>
2018-08-23 13:30:41 +0200

    Select finished

--- client/src/app/components/hand-button/hand-button.component.ts
+++ client/src/app/components/hand-button/hand-button.component.ts
@@ -1,13 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-hand-button',
-  templateUrl: './hand-button.component.html',
-  styleUrls: ['./hand-button.component.css']
+  selector: "app-hand-button",
+  templateUrl: "./hand-button.component.html",
+  styleUrls: ["./hand-button.component.css"]
 })
 export class HandButtonComponent implements OnInit {
-
   public lottieConfig: Object;
   private anim: any;
   private animationSpeed: number = 1;
@@ -15,20 +14,22 @@ export class HandButtonComponent implements OnInit {
     { name: "Web Development Full-time" },
     { name: "Web Development Part-time" },
     { name: "UX/UI Full-time" },
-    { name: "UX/UI Part-time"}
+    { name: "UX/UI Part-time" }
   ];
   public selectedValue = "";
   public color = false;
 
-  constructor(public router:Router) {
-      this.lottieConfig = {
-          path: '../../../assets/handButton/pushButton.json',
-          autoplay: false,
-          loop: false
-      };
+  constructor(public router: Router) {
+    this.lottieConfig = {
+      path: "../../../assets/handButton/pushButton.json",
+      autoplay: false,
+      loop: false
+    };
+
+    
   }
 
-  onClick(){
+  onClick() {
     this.color = true;
   }
 
@@ -41,29 +42,64 @@ export class HandButtonComponent implements OnInit {
     }
   }
 
-  ngOnInit(){
-
+  ngOnInit() {
   }
 
   handleAnimation(anim: any) {
-      this.anim = anim;
+    this.anim = anim;
   }
 
   stop() {
-      this.anim.stop();
+    this.anim.stop();
   }
 
   play() {
-      this.anim.play();
+    this.anim.play();
   }
 
   pause() {
-      this.anim.pause();
+    this.anim.pause();
   }
 
   setSpeed(speed: number) {
-      this.animationSpeed = speed;
-      this.anim.setSpeed(speed);
+    this.animationSpeed = speed;
+    this.anim.setSpeed(speed);
   }
 
+  highlightOption(select, option) {
+    let optionList = select.querySelectorAll(".option");
+
+    optionList.forEach(function(other) {
+      other.classList.remove("highlight");
+    });
+
+    option.classList.add("highlight");
+  }
+
+  toggleOptList(select) {
+    /* let optList = select.querySelector(".optList");
+    optList.classList.toggle('hidden'); */
+  }
+
+  deactivateSelect(select) {
+    if (!select.classList.contains("active")) return;
+
+    let optList = select.querySelector(".optList");
+
+    optList.classList.add("hidden");
+    select.classList.remove("active");
+  }
+
+  activeSelect(select) {
+    if (select.classList.contains("active")) return;
+
+    select.classList.add("active");
+  }
+
+  prueba(i) {
+    let value = document.querySelector(".value");
+    value.innerHTML = i;
+    this.color = true;
+    this.selectedValue = i;
+  }
 }
