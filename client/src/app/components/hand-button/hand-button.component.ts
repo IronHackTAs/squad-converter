d8b5f84ceb0188c2127da64aa7da9d8075cd75bf
Juan Sánchez Mesa <sanchezmesaj@gmail.com>
2018-08-23 17:14:34 +0200

    Animation change

--- client/src/app/components/hand-button/hand-button.component.ts
+++ client/src/app/components/hand-button/hand-button.component.ts
@@ -8,6 +8,9 @@ import { Router } from "@angular/router";
 })
 export class HandButtonComponent implements OnInit {
   public lottieConfig: Object;
+  public lottieConfig2: Object;
+  public displayAnimation: string = "none";
+  public displayAnimation2: string = "block";
   private anim: any;
   private animationSpeed: number = 1;
   public campus = [
@@ -25,6 +28,11 @@ export class HandButtonComponent implements OnInit {
       autoplay: true,
       loop: true
     };
+    this.lottieConfig2 = {
+      path: "../../../assets/handButton/pushButton.json",
+      autoplay: false,
+      loop: false
+    };
   }
 
   onClick() {
@@ -32,12 +40,8 @@ export class HandButtonComponent implements OnInit {
   }
 
   onConfirm() {
-    this.lottieConfig = {
-      path: "../../../assets/handButton/pushButton.json",
-      autoplay: false,
-      loop: false
-    };
-
+    this.displayAnimation = "block";
+    this.displayAnimation2 = "none";
     if (this.selectedValue != "") {
       this.play();
       setTimeout(() => {
@@ -71,7 +75,7 @@ export class HandButtonComponent implements OnInit {
 
   toggleOptList(select) {
     let optList = select.querySelector(".optList");
-    optList.classList.toggle('hidden');
+    optList.classList.toggle("hidden");
   }
 
   prueba(course) {
