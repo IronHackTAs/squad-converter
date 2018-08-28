68a6475294bf3edff0dab37dfd48e80879b61d48
Giorgio Grassini <giorgiograssini@gmail.com>
2018-08-28 13:07:24 +0200

    validation in jeep 🚓

--- client/src/app/components/jeep/jeep.component.ts
+++ client/src/app/components/jeep/jeep.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, Output, Input } from "@angular/core";
 import { Router, Data } from "@angular/router";
 import { DataService } from "../../services/data.service";
 
-
 @Component({
   selector: "app-jeep",
   templateUrl: "./jeep.component.html",
@@ -12,11 +11,14 @@ export class JeepComponent implements OnInit {
   public lottieConfig: Object;
   private anim: any;
   private animationSpeed: number = 1;
-  public name:string = "";
-  public surname:string = "";
-  public color:boolean = false;
+  public name: string = "";
+  public surname: string = "";
+  public color = false;
+  public submit = true;
+  public missingName = false;
+  public missingSurname = false;
 
-  constructor(public route:Router, public data: DataService) {
+  constructor(public route: Router, public data: DataService) {
     this.lottieConfig = {
       path: "../../../assets/jeep/jeep.json",
       autoplay: false,
@@ -28,20 +30,36 @@ export class JeepComponent implements OnInit {
 
   onNameChange(name: string) {
     this.name = name;
-    this.name !== '' && this.surname !== '' ? this.color = true : this.color = false;
+    this.name !== "" && this.surname !== ""
+      ? (this.color = true)
+      : (this.color = false);
+    if(this.name !== '')this.missingName = false;
   }
 
   onSurnameChange(surname: string) {
     this.surname = surname;
-    this.name !== '' && this.surname !== '' ? this.color = true : this.color = false;
+    this.name !== "" && this.surname !== ""
+      ? (this.color = true)
+      : (this.color = false);
+      if(this.surname !== '')this.missingSurname = false;
   }
 
   onConfirm() {
-    this.data.addName(this.name,this.surname);
+    if (this.name !== "" && this.surname !== "") {
+      this.submit = false;
+      this.data.addName(this.name, this.surname);
       this.play();
-      setTimeout (()=>{
-          this.route.navigate(['/boarding'])
-      },3000)
+      setTimeout(() => {
+        this.route.navigate(["/boarding"]);
+      }, 3000);
+    } else if (this.name == '' && this.surname == '') {
+      this.missingName = true;
+      this.missingSurname = true;
+    } else if (this.surname == ''){
+      this.missingSurname = true;
+    } else if (this.name == '') {
+      this.missingName = true;
+    }
   }
 
   handleAnimation(anim: any) {
