ebd5d3a873a43b329ec211fab2e4f147d58fa5af
Giorgio Grassini <giorgiograssini@gmail.com>
2018-09-04 12:53:48 +0200

    media 1440px 🎨

--- server/app.js
+++ server/app.js
@@ -80,8 +80,5 @@ app.use('/', index);
 
 const authRoutes = require('./routes/auth');
 app.use('/auth', authRoutes);
-      
-const crud = require('./routes/crud');
-app.use('/crud',crud);
 
 module.exports = app;
