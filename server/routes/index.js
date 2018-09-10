e7198bb564fc55edaed82fb36342758382a44203
Giorgio Grassini <giorgiograssini@gmail.com>
2018-09-10 21:45:50 +0200

    linkedin working ✨

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router  = express.Router();
 
+
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+const linkedinRoute = require('./api/linkedin');
+router.use('/api', linkedinRoute);
 
 module.exports = router;
