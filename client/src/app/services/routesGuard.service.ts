5045bffa1f1c4a1132a7bb35e2b33a5db0e0f8b2
diegomendezp <diegomen15@gmail.com>
2018-11-16 12:02:06 +0100

    Route Guard created 👮‍♂️

---
+++ client/src/app/services/routesGuard.service.ts
@@ -0,0 +1,24 @@
+import {CanActivate,ActivatedRouteSnapshot,RouterStateSnapshot, Router} from "../../../node_modules/@angular/router";
+import { Injectable } from "../../../node_modules/@angular/core";
+import { Observable } from "../../../node_modules/rxjs";
+import { DataService } from "./data.service";
+
+@Injectable({
+  providedIn:"root"
+})
+export class isLoggedGuardService implements CanActivate {
+  constructor(public dataService:DataService, public router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    router: RouterStateSnapshot
+  ): boolean {
+    if(this.dataService.getData().name !== '' || this.dataService.getData().email !== ''){
+      return true
+    } else {
+      this.router.navigate(['/jeep'])
+      return false
+    }
+  
+  }
+}
