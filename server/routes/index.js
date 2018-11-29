52a084144c4e0f6f845969915d1bdbbbdf097413
Giorgio Grassini <giorgiograssini@gmail.com>
2018-11-29 22:17:21 +0100

    Changed img

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,9 @@
+
+const linkedinRoute = require('./api/linkedin');
+const databaseRoute = require('./api/database');
 const express = require('express');
 
 const router = express.Router();
-const linkedinRoute = require('./api/linkedin');
-const databaseRoute = require('./api/database');
 
 router.use('/api', linkedinRoute);
 router.use('/api/database', databaseRoute);
